67d81dd2edf77052c58242761eeb714938ac6184
haiqiang-zhang <1552094108@qq.com>
2021-12-16 16:53:07 +0800

    improve solder side peference

--- SecondHand/views.py
+++ SecondHand/views.py
@@ -25,10 +25,6 @@ from .model import Items, Items_del
 SecondHand_bp = Blueprint("SecondHand_bp", __name__, template_folder="templates")
 
 
-
-
-
-
 @SecondHand_bp.before_request
 def check_fresh_login():
     """Checks if the login is fresh for the current user, otherwise the user
@@ -39,52 +35,6 @@ def check_fresh_login():
 
 @SecondHand_bp.route("/", methods=['GET', 'POST'])
 def SecondHand_index():
-    # page = request.args.get("page", 1, type=int)
-    # forum_ids = []
-    #
-    # plugin = PluginRegistry.query.filter_by(name="SecondHand").first()
-    # if plugin and not plugin.settings:
-    #     flash(
-    #         _(
-    #             "Please install the plugin first to configure the forums "
-    #             "which should be displayed."
-    #         ),
-    #         "warning",
-    #     )
-    # else:
-    #     forum_ids = plugin.settings["forum_ids"]
-    # group_ids = [group.id for group in current_user.groups]
-    # forums = Forum.query.filter(Forum.groups.any(Group.id.in_(group_ids)))
-    #
-    # # get the news forums - check for permissions
-    # news_ids = [f.id for f in forums.filter(Forum.id.in_(forum_ids)).all()]
-    # news = (
-    #     Topic.query.filter(Topic.forum_id.in_(news_ids))
-    #     .order_by(Topic.id.desc())
-    #     .paginate(page, flaskbb_config["TOPICS_PER_PAGE"], True)
-    # )
-    #
-    # # get the recent topics from all to the user available forums (not just the
-    # # configured ones)
-    # all_ids = [f.id for f in forums.all()]
-    # recent_topics = (
-    #     Topic.query.filter(Topic.forum_id.in_(all_ids))
-    #     .order_by(Topic.last_updated.desc())
-    #     .limit(plugin.settings.get("recent_topics", 10))
-    # )
-    #
-    # user_count = User.query.count()
-    # topic_count = Topic.query.count()
-    # post_count = Post.query.count()
-    # newest_user = User.query.order_by(User.id.desc()).first()
-    #
-    # # Check if we use redis or not
-    # if not current_app.config["REDIS_ENABLED"]:
-    #     online_users = User.query.filter(User.lastseen >= time_diff()).count()
-    #     online_guests = None
-    # else:
-    #     online_users = len(get_online_users())
-    #     online_guests = len(get_online_users(guest=True))
     session = SecondHand.Session()
     items = session.query(Items).all()
     user_id = current_user.id
@@ -140,11 +90,25 @@ def SecondHand_del_myRelease(item):
     session.add(item_del)
     session.delete(i)
     session.commit()
-    return redirect(url_for("SecondHand_bp.SecondHand_userRecord"))
+    url = request.args.get("next_url")
+    return redirect(url)
 
 
 @SecondHand_bp.route("/SecondHand_mgmt")
 def SecondHand_mgmt():
     return render_template(
         "SecondHand_mgmt.html"
+    )
+
+
+@SecondHand_bp.route("/SecondHand_desc/<item>")
+def SecondHand_desc(item):
+    session = SecondHand.Session()
+    i: Items = session.query(Items).filter(Items.id == item).one()
+    user = User.query.filter(i.sellerID == User.id).one()
+    return render_template(
+        "SecondHand_itemsDesc.html",
+        item=i,
+        user=user,
+        request=request
     )
